6610aa1b5eaffcaa430785ebd388c2740712f247
Xavier <xaviermunroe@ac.c4q.nyc>
2018-12-05 23:34:20 -0500

    built some more relations. Think I have the scheme right. Going to break up my migrations into separate files.

--- server/migrations/20181205180507_product_table.js
+++ server/migrations/20181205180507_product_table.js
@@ -4,31 +4,49 @@ exports.up = async (knex, Promise) => {
     table.string('category')
   })
 
-  const shoesTable = await knex.schema.createTable('shoe', (table) => {
+  const footwearTable = await knex.schema.createTable('footwear', (table) => {
     table.increments('id').primary()
-    table.integer('product_id').references('product.id')
-    table.string('name')
+    table.integer('product_id').references('product.id').unique()
+    table.string('type')
+  })
+
+  const topTable = await knex.schema.createTable('top', (table) => {
+    table.increments('id').primary()
+    table.integer('product_id').references('product.id').unique()
     table.string('type')
   })
 
   const sneakersTable = await knex.schema.createTable('sneaker', (table) => {
     table.increments('id').primary()
-    table.integer('product_id').references('product.id')
-    table.string('name').references('shoe')
+    table.integer('product_id').references('footwear.product_id').unique()
+    table.string('name')
     table.boolean('male')
     table.boolean('female')
     table.boolean('youth')
-    table.specificType('sizes', 'jsonb[]')
-    table.specificType('colors', 'jsonb[]')
+    table.json('sizes')
+    table.json('colors')
   })
 
-  return Promise.all([productTable, shoesTable, sneakersTable])
+  const shirtTable = await knex.schema.createTable('shirt', (table) => {
+    table.increments('id').primary()
+    table.integer('product_id').references('top.product_id').unique()
+    table.string('name')
+    table.boolean('male')
+    table.boolean('female')
+    table.boolean('youth')
+    table.json('sizes')
+    table.json('colors')
+  })
+
+  return Promise.all([productTable, footwearTable, topTable, sneakersTable, shirtTable])
 };
 
 exports.down = async (knex, Promise) => {
-  const dropProduct = await knex.schema.dropTable('product')
-  const dropShoes = await knex.schema.dropTable('shoe')
-  const dropSneakers = await knex.schema.dropTable('sneaker')
+  const dropShirts = await knex.schema.dropTableIfExists('shirt')
+  const dropSneakers = await knex.schema.dropTableIfExists('sneaker')
+  const dropTops = await knex.schema.dropTableIfExists('top')
+  const dropFootwear = await knex.schema.dropTableIfExists('footwear')
+  const dropProduct = await knex.schema.raw(`DROP TABLE product cascade`)
 
-  return Promise.all([dropProduct, dropShoes, dropSneakers])
+  return Promise.all([dropProduct, dropFootwear, dropTops, dropSneakers, dropShirts])
 };
